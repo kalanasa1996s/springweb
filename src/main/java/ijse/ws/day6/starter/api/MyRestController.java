d4ac81490fe61b83a32786be658002d0dde084cf
kalanasa1996s <kalanasa1996@gmail.com>
2020-10-31 16:13:48 +0530

    Create Message POJO

--- src/main/java/ijse/ws/day6/starter/api/MyRestController.java
+++ src/main/java/ijse/ws/day6/starter/api/MyRestController.java
@@ -1,17 +1,23 @@
 package ijse.ws.day6.starter.api;
 
+import ijse.ws.day6.starter.data.Message;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/api")
-public class MyRestController {
-    @GetMapping(path = "/hello", produces = "text/plain")
-    public ResponseEntity<Object> sayHello() {
 
-        return new ResponseEntity<Object>("Hello World", HttpStatus.OK);
+import java.util.Date;
+
+@RestController
+public class MyRestController{
+    @GetMapping("/api/hello")
+    public Message greeting(){
+        Message msg = new Message();
+        msg.setSender("Server");
+        msg.setMessage("Hello Spring");
+        msg.setSentTime(new Date());
+        return msg;
     }
 }
